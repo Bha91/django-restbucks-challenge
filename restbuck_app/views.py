6ddfb2f5ba10795fccdac7acb3fac87c56ba2cfc
Behnam <bha.1370@yahoo.com>
2021-03-15 14:01:50 +0330

    change | Views: integrate get, post, delete to extract method

--- restbuck_app/views.py
+++ restbuck_app/views.py
@@ -52,8 +52,8 @@ class OrderView(APIView):
         :type pk: int
         :return: API response data and status code
         """
-        user = get_auth_user(request)
         data = []
+        user = get_auth_user(request)
         if pk > 0:
             order, response_status = self.get_object(pk, user)
             if response_status == status.HTTP_404_NOT_FOUND:
@@ -108,24 +108,23 @@ class OrderView(APIView):
         """
         # TODO: check product feature possibility
         user = get_auth_user(request)
-        tobe_deleted = None
         if pk > 0:
             order, response_status = self.get_object(pk, user)
-            tobe_deleted = ProductOrder.objects.filter(order=order)
             if response_status == status.HTTP_404_NOT_FOUND:
                 return Response({'error': True, 'message': 'requested order dose not exist'}, response_status)
             elif response_status == status.HTTP_403_FORBIDDEN:
                 return Response({'error': True, 'message': 'Not your order'}, response_status)
-        elif pk == 0:
-            order = Order.objects.create(user=user)
-        else:
+        elif pk < 0:
             return Response({'error': True, 'message': 'Not valid order id'}, status.HTTP_400_BAD_REQUEST)
+        else:
+            order = Order.objects.create(user=user)
 
         data = request.data.get('data')
         serializer = ProductOrderFlatSerializer(data=data, many=True)
         if serializer.is_valid():
             # TODO: must be changed for production, specially without log, it is dangerous
-            if tobe_deleted is not None:
+            tobe_deleted = ProductOrder.objects.filter(order=order)
+            if tobe_deleted.exists():
                 tobe_deleted.delete()
             serializer.save(order=order)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
