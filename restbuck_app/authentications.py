6ab74384667296d39cf3bc0040c11ec2ebf0b3fe
Behnam <bha.1370@yahoo.com>
2021-04-03 15:54:23 +0430

    delete | authentications, not used

--- restbuck_app/authentications.py
+++
@@ -1,7 +0,0 @@
-""" Customized authentication classes for restbuck_app application. """
-from rest_framework import authentication
-
-
-class BearerAuthentication(authentication.TokenAuthentication):
-    keyword = 'Bearer'
-
